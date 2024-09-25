024d6af24a6165594bbb3379bc5d86f061ec6418
Dias <dias.atudinov@gmail.com>
2024-09-25 13:12:54 +0500

    Start of project

---
+++ app947/app947/app947App.swift
@@ -0,0 +1,17 @@
+//
+//  app947App.swift
+//  app947
+//
+//  Created by Dias Atudinov on 25.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct app947App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
