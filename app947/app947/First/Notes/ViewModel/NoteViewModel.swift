14761acb15153718752154a4ae2f679e09ba66b9
Dias <dias.atudinov@gmail.com>
2024-10-01 01:03:52 +0500

    Note View Model

--- app947/app947/First/Notes/ViewModel/NoteViewModel.swift
+++ app947/app947/First/Notes/ViewModel/NoteViewModel.swift
@@ -10,4 +10,15 @@ import Foundation
 class NoteViewModel: ObservableObject {
     
     @Published var notes: [Note] = [Note(name: "Botanical Garden", location: "Russia, Moscow", date: Date(), note: "A unique garden with a variety of plants from all over the world. Ideal place for privacy and quiet walks among nature")]
+    
+    func addNote(_ note: Note) {
+        notes.append(note)
+        
+    }
+    
+    func deleteNote(_ note: Note) {
+        if let index = notes.firstIndex(where: { $0.id == note.id }) {
+            notes.remove(at: index)
+        }
+    }
 }
